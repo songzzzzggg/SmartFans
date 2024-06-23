114fe470f04064e145831eda601156b61a1dc593
songzuogang <1170031164@qq.com>
2024-06-23 15:52:16 +0800

    删除没用的文件

--- NotifyListenerDemo/app/src/main/java/com/example/notifylistenerdemo/MainActivity.java
+++ NotifyListenerDemo/app/src/main/java/com/example/notifylistenerdemo/MainActivity.java
@@ -1,8 +1,5 @@
 package com.example.notifylistenerdemo;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.bluetooth.BluetoothAdapter;
@@ -13,47 +10,31 @@ import android.bluetooth.BluetoothGattCharacteristic;
 import android.bluetooth.le.BluetoothLeScanner;
 import android.bluetooth.le.ScanCallback;
 import android.bluetooth.le.ScanResult;
-import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.IntentFilter;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
-
-import androidx.core.app.ActivityCompat;
-import androidx.core.app.NotificationManagerCompat;
-
-import android.content.ComponentName;
-import android.content.Intent;
-
-import android.content.pm.PackageManager;
-
 import android.os.Handler;
 import android.os.Message;
-import android.service.notification.StatusBarNotification;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Locale;
-import java.util.Set;
-import java.util.UUID;
-
-import static com.example.notifylistenerdemo.NotifyHelper.*;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.example.notifylistenerdemo.ble.BLEManager;
-import com.example.notifylistenerdemo.ble.OnBleConnectListener;
-import com.example.notifylistenerdemo.ble.OnDeviceSearchListener;
 import com.example.notifylistenerdemo.permission.PermissionListener;
 import com.example.notifylistenerdemo.permission.PermissionRequest;
 
-public class MainActivity extends AppCompatActivity implements NotifyListener {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class MainActivity extends AppCompatActivity{
 
     private static final int REQUEST_CODE = 9527;
     private TextView textView;
@@ -96,8 +77,6 @@ public class MainActivity extends AppCompatActivity implements NotifyListener {
     private static final int BT_CLOSED = 0x0E;
     private static final int SET_TEXT = 0x0F;
 
-//    private LVDevicesAdapter lvDevicesAdapter;
-//    private ListView lvDevices;
 
 
     private Context mContext;
@@ -248,11 +227,9 @@ public class MainActivity extends AppCompatActivity implements NotifyListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        requestPermission();
         mContext = this;
         // 初始化视图
         initView();
-        NotifyHelper.getInstance().setNotifyListener(this);
 
         // 检查蓝牙是否可用
         if (!checkBLE()) {
@@ -347,173 +324,6 @@ public class MainActivity extends AppCompatActivity implements NotifyListener {
 
 
     /**
-     * 请求权限
-     *
-     * @param view
-     */
-    public void requestPermission(View view) {
-        if (!isNLServiceEnabled()) {
-            Intent intent = new Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS");
-            startActivityForResult(intent, REQUEST_CODE);
-        } else {
-            showMsg("通知服务已开启");
-            toggleNotificationListenerService();
-        }
-    }
-
-    /**
-     * 请求权限
-     *
-     */
-    public void requestPermission() {
-        if (!isNLServiceEnabled()) {
-            Intent intent = new Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS");
-            startActivityForResult(intent, REQUEST_CODE);
-        } else {
-            showMsg("通知服务已开启");
-            toggleNotificationListenerService();
-        }
-    }
-
-    /**
-     * 是否启用通知监听服务
-     *
-     * @return
-     */
-    public boolean isNLServiceEnabled() {
-        Set<String> packageNames = NotificationManagerCompat.getEnabledListenerPackages(this);
-        if (packageNames.contains(getPackageName())) {
-            return true;
-        }
-        return false;
-    }
-
-    /**
-     * 切换通知监听器服务
-     */
-    public void toggleNotificationListenerService() {
-        PackageManager pm = getPackageManager();
-        pm.setComponentEnabledSetting(new ComponentName(getApplicationContext(), NotifyService.class),
-                PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
-
-        pm.setComponentEnabledSetting(new ComponentName(getApplicationContext(), NotifyService.class),
-                PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP);
-    }
-
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_CODE) {
-            if (isNLServiceEnabled()) {
-                showMsg("通知服务已开启");
-                toggleNotificationListenerService();
-            } else {
-                showMsg("通知服务未开启");
-            }
-        }
-    }
-
-
-    private void showMsg(String msg) {
-        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
-    }
-
-    /**
-     * 收到通知
-     *
-     * @param type 通知类型
-     */
-    @Override
-    public void onReceiveMessage(int type) {
-        switch (type) {
-            case N_MESSAGE:
-                textView.setText("收到短信消息");
-                //震动一下，通过蓝牙传递一个特征值，表名这是消息
-                characteristic.setValue("1");
-                break;
-            case N_CALL:
-                textView.setText("收到来电消息");
-                //震动一下，通过蓝牙传递一个特征值，表名这是电话
-                characteristic.setValue("0");
-                break;
-            case N_WX:
-                textView.setText("收到微信消息");
-                characteristic.setValue("1");
-                break;
-            case N_QQ:
-                textView.setText("收到QQ消息");
-                characteristic.setValue("1");
-//                this.isInformation=true;
-                //将this.isInformation通过某种渠道传递给蓝牙
-                break;
-            default:
-                break;
-        }
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
-            return;
-        }
-        bluetoothGatt.writeCharacteristic(characteristic);
-    }
-
-    /**
-     * 移除通知
-     *
-     * @param type 通知类型
-     */
-    @Override
-    public void onRemovedMessage(int type) {
-        switch (type) {
-            case N_MESSAGE:
-                textView.setText("移除短信消息");
-                break;
-            case N_CALL:
-                textView.setText("移除来电消息");
-                break;
-            case N_WX:
-                textView.setText("移除微信消息");
-                break;
-            case N_QQ:
-                textView.setText("移除QQ消息");
-                break;
-            default:
-                break;
-        }
-    }
-
-    /**
-     * 收到通知
-     *
-     * @param sbn 状态栏通知
-     */
-    @Override
-    public void onReceiveMessage(StatusBarNotification sbn) {
-        if (sbn.getNotification() == null) return;
-        //消息内容
-        String msgContent = "";
-        if (sbn.getNotification().tickerText != null) {
-            msgContent = sbn.getNotification().tickerText.toString();
-        }
-
-        //消息时间
-        String time = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.CHINESE).format(new Date(sbn.getPostTime()));
-        textView.setText(String.format(Locale.getDefault(),
-                "应用包名：%s\n消息内容：%s\n消息时间：%s\n",
-                sbn.getPackageName(), msgContent, time));
-    }
-
-    /**
-     * 移除通知
-     *
-     * @param sbn 状态栏通知
-     */
-    @Override
-    public void onRemovedMessage(StatusBarNotification sbn) {
-        textView.setText("通知移除");
-    }
-
-
-    /**
      * 初始化视图
      */
     private void initView() {
@@ -546,28 +356,9 @@ public class MainActivity extends AppCompatActivity implements NotifyListener {
         }
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-    }
 
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.btnConnBLE:
-                if (bluetoothGatt != null) {
-                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
-                        return;
-                    }
-                    bluetoothDevice.connectGatt(this, true, gattCallback);
-                }
-                break;
-            default:
-                break;
-        }
-    }
+
     public void onClickFanControl(View view) {
-//        System.out.println("点击了一次风扇控制按钮");
         //点击风扇控制按钮实现跳转
         startActivity(new Intent(MainActivity.this,FanControlActivity.class));
     }
