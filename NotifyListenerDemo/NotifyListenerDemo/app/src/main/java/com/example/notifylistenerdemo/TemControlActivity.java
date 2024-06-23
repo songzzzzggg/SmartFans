92e5a0463f125ceeb71fa8a747e397471175b921
songzuogang <1170031164@qq.com>
2024-06-23 15:45:40 +0800

    改造MainActivity，删掉没用的部分

---
+++ NotifyListenerDemo/NotifyListenerDemo/app/src/main/java/com/example/notifylistenerdemo/TemControlActivity.java
@@ -0,0 +1,31 @@
+package com.example.notifylistenerdemo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+//温度控制界面逻辑
+public class TemControlActivity extends AppCompatActivity {
+
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.temcontrol_activity);
+    }
+
+    public void onClickFanControl(View view) {
+        //点击温度控制按钮实现跳转
+        startActivity(new Intent(TemControlActivity.this, FanControlActivity.class));
+    }
+
+    public void onClickFanStatus(View view) {
+        //点击风扇状态按钮实现跳转
+        startActivity(new Intent(TemControlActivity.this, MainActivity.class));
+    }
+}
